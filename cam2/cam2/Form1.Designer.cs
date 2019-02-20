ad1dfdeb2a65ba4f35068854a1634e22a3409d8f
Fray <941013928@qq.com>
2019-02-20 18:05:55 +0800

    cam2 add canny消抖

--- cam2/cam2/Form1.Designer.cs
+++ cam2/cam2/Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.components = new System.ComponentModel.Container();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
             this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cannybox = new System.Windows.Forms.TextBox();
+            this.canny_th_up = new System.Windows.Forms.Button();
+            this.canny_th_down = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
             this.SuspendLayout();
@@ -39,23 +43,67 @@
             // 
             this.imageBox1.Location = new System.Drawing.Point(12, 12);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(577, 409);
+            this.imageBox1.Size = new System.Drawing.Size(720, 480);
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
             // 
             // imageBox2
             // 
-            this.imageBox2.Location = new System.Drawing.Point(606, 12);
+            this.imageBox2.Location = new System.Drawing.Point(766, 12);
             this.imageBox2.Name = "imageBox2";
-            this.imageBox2.Size = new System.Drawing.Size(577, 409);
+            this.imageBox2.Size = new System.Drawing.Size(720, 480);
             this.imageBox2.TabIndex = 2;
             this.imageBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 20F);
+            this.label1.Location = new System.Drawing.Point(12, 516);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 34);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Canny\'s Thresh:";
+            // 
+            // cannybox
+            // 
+            this.cannybox.Font = new System.Drawing.Font("宋体", 20F);
+            this.cannybox.Location = new System.Drawing.Point(324, 513);
+            this.cannybox.Name = "cannybox";
+            this.cannybox.Size = new System.Drawing.Size(113, 46);
+            this.cannybox.TabIndex = 4;
+            // 
+            // canny_th_up
+            // 
+            this.canny_th_up.Font = new System.Drawing.Font("宋体", 15F);
+            this.canny_th_up.Location = new System.Drawing.Point(479, 519);
+            this.canny_th_up.Name = "canny_th_up";
+            this.canny_th_up.Size = new System.Drawing.Size(90, 40);
+            this.canny_th_up.TabIndex = 5;
+            this.canny_th_up.Text = "up";
+            this.canny_th_up.UseVisualStyleBackColor = true;
+            this.canny_th_up.Click += new System.EventHandler(this.canny_th_up_Click);
+            // 
+            // canny_th_down
+            // 
+            this.canny_th_down.Font = new System.Drawing.Font("宋体", 15F);
+            this.canny_th_down.Location = new System.Drawing.Point(592, 519);
+            this.canny_th_down.Name = "canny_th_down";
+            this.canny_th_down.Size = new System.Drawing.Size(90, 40);
+            this.canny_th_down.TabIndex = 6;
+            this.canny_th_down.Text = "down";
+            this.canny_th_down.UseVisualStyleBackColor = true;
+            this.canny_th_down.Click += new System.EventHandler(this.canny_th_down_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1195, 463);
+            this.ClientSize = new System.Drawing.Size(1532, 753);
+            this.Controls.Add(this.canny_th_down);
+            this.Controls.Add(this.canny_th_up);
+            this.Controls.Add(this.cannybox);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.imageBox2);
             this.Controls.Add(this.imageBox1);
             this.Name = "Form1";
@@ -63,6 +111,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +119,10 @@
 
         private Emgu.CV.UI.ImageBox imageBox1;
         private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox cannybox;
+        private System.Windows.Forms.Button canny_th_up;
+        private System.Windows.Forms.Button canny_th_down;
     }
 }
 
--- cam2/cam2/Form1.cs
+++ cam2/cam2/Form1.cs
@@ -14,6 +14,11 @@ namespace cam2
     public partial class Form1 : Form
     {
         private static Capture _cameraCapture;
+        private Image<Bgr, Byte> frame ;
+        private Image<Gray, Byte>[] canny_frame = new Image<Gray,Byte>[2];
+        private Image<Gray, Byte> canny_out ;
+        static int cnt = 0;//消抖计数器
+        static int th = 100;//canny第二阈值初始化
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +28,7 @@ namespace cam2
         {
             try
             {
-                _cameraCapture = new Capture();
+                _cameraCapture = new Capture(1);
             }
             catch (Exception e)
             {
@@ -34,9 +39,47 @@ namespace cam2
         }
         void ProcessFrame(object sender, EventArgs e)
         {
-            Image<Bgr, Byte> frame = _cameraCapture.QueryFrame();
-            frame._SmoothGaussian(3); //filter out noises
+            frame = _cameraCapture.QueryFrame();
+            canny_out = frame.Convert<Gray, Byte>();
+            //frame._SmoothGaussian(3); //filter out noises
+            if (cnt<1)
+            {
+                canny_frame[cnt] = _cameraCapture.QueryFrame().Canny(50, th);
+                cnt++;
+            }
+            else
+            {
+                canny_frame[cnt] = _cameraCapture.QueryFrame().Canny(50, th);
+                for(int i=0;i<imageBox1.Height;i++)
+                {
+                    for(int j=0;j<imageBox1.Width;j++)
+                    {
+                        if(canny_frame[0].Data[i,j,0]==canny_frame[1].Data[i,j,0])
+                        {
+                            canny_out.Data[i, j, 0] = canny_frame[0].Data[i, j, 0];
+                        }
+                        else
+                        {
+                            canny_out.Data[i, j, 0] = 0;
+                        }
+                    }
+                }
+                imageBox2.Image = canny_out;
+                cnt = 0;
+            }
             imageBox1.Image = frame;
+            //imageBox2.Image = frame.Canny(50, th);
+            cannybox.Text = Convert.ToString(th);
+        }
+
+        private void canny_th_up_Click(object sender, EventArgs e)
+        {
+            th++;
+        }
+
+        private void canny_th_down_Click(object sender, EventArgs e)
+        {
+            th--;
         }
     }
 }
