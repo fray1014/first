bafc16fdcb67466aad9ed2211606abf39f97f21c
Fray <941013928@qq.com>
2019-03-19 16:39:42 +0800

    添加图标

--- cam2/cam2/Program.cs
+++ cam2/cam2/Program.cs
@@ -15,7 +15,7 @@ namespace cam2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new roiDetection());
         }
     }
 }
